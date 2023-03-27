e1921bbca1de55b2bb0d6e1d631ff375187df453
Gin_99 <3348483879@qq.com>
2023-03-27 22:05:19 +0800

    修改路由

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,32 +1,16 @@
 const routes = [
     {
         path: '/',
-        component: () => import('../views/Home.vue')
+        component: () => import('../views/home.vue')
     },
     {
         path: '/setting',
         component: () => import('../views/Setting.vue')
     },
     {
-        path: '/works',
-        component: () => import('../views/Works.vue')
-    },
-    {
         path: '/login',
         meta: { show: true },
         component: () => import('../views/Login.vue')
-    },
-    {
-        path: '/competition',
-        component: () => import('../views/Competition.vue')
-    },
-    {
-        path: '/score',
-        component: () => import('../views/Score.vue')
-    },
-    {
-        path: '/notify',
-        component: () => import('../views/Notify.vue')
     },
 ]
 
