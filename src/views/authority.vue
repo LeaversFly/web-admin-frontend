307ec68ad95062e1181427052c3fcd34b2a36543
Gin_99 <3348483879@qq.com>
2023-03-29 22:02:16 +0800

    完善权限管理页面

--- src/views/authority.vue
+++ src/views/authority.vue
@@ -1,57 +1,37 @@
 <template>
     <el-table :data="filterTableData" style="width: 100%">
-        <el-table-column label="Date" prop="date" />
-        <el-table-column label="Name" prop="name" />
+        <el-table-column label="用户id" prop="user_id" />
+        <el-table-column label="剩余次数" prop="remain" />
+        <el-table-column label="主题" prop="theme" />
         <el-table-column align="right">
             <template #header>
-                <el-input v-model="search" size="small" placeholder="Type to search" />
+                <el-input v-model="search" size="small" placeholder="点击搜索..." />
             </template>
             <template #default="scope">
-                <el-button size="small" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
-                <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+                <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">禁用</el-button>
             </template>
         </el-table-column>
     </el-table>
 </template>
 
 <script setup>
+import useStore from '../store'
+
+const { userStore } = useStore()
+
 const search = ref('')
-const filterTableData = computed(() =>
-    tableData.filter(
-        (data) =>
-            !search.value ||
-            data.name.toLowerCase().includes(search.value.toLowerCase())
-    )
-)
-const handleEdit = (index, row) => {
-    console.log(index, row)
-}
+
+// const filterTableData = computed(() =>
+//     tableData.filter(
+//         (data) =>
+//             !search.value ||
+//             data.name.toLowerCase().includes(search.value.toLowerCase())
+//     )
+// )
+
 const handleDelete = (index, row) => {
     console.log(index, row)
 }
-
-const tableData = [
-    {
-        date: '2016-05-03',
-        name: 'Tom',
-        address: 'No. 189, Grove St, Los Angeles',
-    },
-    {
-        date: '2016-05-02',
-        name: 'John',
-        address: 'No. 189, Grove St, Los Angeles',
-    },
-    {
-        date: '2016-05-04',
-        name: 'Morgan',
-        address: 'No. 189, Grove St, Los Angeles',
-    },
-    {
-        date: '2016-05-01',
-        name: 'Jessy',
-        address: 'No. 189, Grove St, Los Angeles',
-    },
-]
 </script>
 
 <style></style>
