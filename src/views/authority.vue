c9f67bce0c142823bf94bed3bcc8598768156b7a
Gin_99 <3348483879@qq.com>
2023-03-31 23:27:03 +0800

    新增筛选用户

--- src/views/authority.vue
+++ src/views/authority.vue
@@ -45,7 +45,7 @@ const handleForbiden = (index, row) => {
 <style lang="scss" scoped>
 .container {
     margin: 20px;
-    padding: 8px;
+    padding: 0 8px 8px 8px;
 
     border: 0.5px solid #e0e0e0;
     box-shadow: 0 2px 10px 0 rgba(0 0 0 / 5%);
