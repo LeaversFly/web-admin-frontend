77b70ba4191f7f3a9141fbe1777e752f08618f22
Gin_99 <3348483879@qq.com>
2023-04-03 23:09:07 +0800

    新增接口

--- src/api/user.js
+++ src/api/user.js
@@ -3,3 +3,7 @@ import axios from './interceptors'
 export function getUserData() {
     return axios.get('/user/all')
 }
+
+export function updateRemainById(data) {
+    return axios.put('/user/edit', data)
+}
