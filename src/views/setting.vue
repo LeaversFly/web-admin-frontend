b6d3d573cac4bfcc2e3d69868a7551051278aa9d
Gin_99 <3348483879@qq.com>
2023-04-08 23:43:37 +0800

    新增设置页面

---
+++ src/views/setting.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.container {
+    margin: 20px;
+    padding: 0 8px 8px 8px;
+
+    border: 0.5px solid #e0e0e0;
+    box-shadow: 0 2px 10px 0 rgba(0 0 0 / 5%);
+    border-radius: 12px;
+}
+</style>
