4718359e93d9c2c60c741095e7c78ae57cac08a9
Gin_99 <3348483879@qq.com>
2023-04-10 23:00:42 +0800

    完善设置页

--- src/views/setting.vue
+++ src/views/setting.vue
@@ -1,70 +1,26 @@
 <template>
     <div class="container">
-        <el-form :model="form" label-width="120px">
-            <el-form-item label="Activity name">
-                <el-input v-model="form.name" />
-            </el-form-item>
-            <el-form-item label="Activity zone">
-                <el-select v-model="form.region" placeholder="please select your zone">
-                    <el-option label="Zone one" value="shanghai" />
-                    <el-option label="Zone two" value="beijing" />
-                </el-select>
-            </el-form-item>
-            <el-form-item label="Activity time">
-                <el-col :span="11">
-                    <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%" />
-                </el-col>
-                <el-col :span="2" class="text-center">
-                    <span class="text-gray-500">-</span>
-                </el-col>
-                <el-col :span="11">
-                    <el-time-picker v-model="form.date2" placeholder="Pick a time" style="width: 100%" />
-                </el-col>
-            </el-form-item>
-            <el-form-item label="Instant delivery">
-                <el-switch v-model="form.delivery" />
-            </el-form-item>
-            <el-form-item label="Activity type">
-                <el-checkbox-group v-model="form.type">
-                    <el-checkbox label="Online activities" name="type" />
-                    <el-checkbox label="Promotion activities" name="type" />
-                    <el-checkbox label="Offline activities" name="type" />
-                    <el-checkbox label="Simple brand exposure" name="type" />
-                </el-checkbox-group>
-            </el-form-item>
-            <el-form-item label="Resources">
-                <el-radio-group v-model="form.resource">
-                    <el-radio label="Sponsor" />
-                    <el-radio label="Venue" />
-                </el-radio-group>
-            </el-form-item>
-            <el-form-item label="Activity form">
-                <el-input v-model="form.desc" type="textarea" />
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="onSubmit">Create</el-button>
-                <el-button>Cancel</el-button>
+        <el-form label-width="140px">
+            <el-form-item label="手动清理">
+                <el-button type="danger" round :icon="deleteIcon" @click="clear"></el-button>
             </el-form-item>
         </el-form>
     </div>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import useStore from '../store'
+import { messageBox } from '../utils/message'
 
-const form = reactive({
-    name: '',
-    region: '',
-    date1: '',
-    date2: '',
-    delivery: false,
-    type: [],
-    resource: '',
-    desc: '',
-})
+const { fileStore } = useStore()
 
-const onSubmit = () => {
-    console.log('submit!')
+const deleteIcon = 'Delete'
+
+const clear = () => {
+    let res = fileStore.deleteAllFile()
+    if (res) {
+        messageBox({ message: '删除成功', type: 'success' })
+    }
 }
 </script>
 
@@ -76,5 +32,11 @@ const onSubmit = () => {
     border: 0.5px solid #e0e0e0;
     box-shadow: 0 2px 10px 0 rgba(0 0 0 / 5%);
     border-radius: 12px;
+
+    .el-form {
+        .el-form-item {
+            margin: 20px 0;
+        }
+    }
 }
 </style>
